d77cee2758f27480040e2c6bae55fd0890136a66
TracyAntonia <aantonjuguna@gmail.com>
2023-06-18 23:50:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+const message = []
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++) {
+        message[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message 
+}
+
+ function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+ }
